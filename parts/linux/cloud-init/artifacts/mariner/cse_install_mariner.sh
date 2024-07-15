99919cd4e5a89033640761a45c458a3e251fbc63
Mitch Zhu <mitchzhu@microsoft.com>
2024-07-15 23:12:17 +0000

    Add --rpmverbosity= option to kata-packages-host installation for debug

--- parts/linux/cloud-init/artifacts/mariner/cse_install_mariner.sh
+++ parts/linux/cloud-init/artifacts/mariner/cse_install_mariner.sh
@@ -27,7 +27,7 @@ installDeps() {
 
 installKataDeps() {
     if [[ $OS_VERSION != "1.0" ]]; then
-      if ! dnf_install 30 1 600 kata-packages-host; then
+      if ! dnf_install --rpmverbosity= option 30 1 600 kata-packages-host; then
         exit $ERR_APT_INSTALL_TIMEOUT
       fi
     fi
