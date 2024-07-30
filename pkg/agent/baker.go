b4db2200c50f311a3541a96da02a8b903b33eaa3
Jiaxiao Zhou (Mossaka) <duibao55328@gmail.com>
2024-07-30 16:19:50 -0700

    bump the spin shim to v0.15.1 from spinkube
    Signed-off-by: Jiaxiao Zhou (Mossaka) <duibao55328@gmail.com>

--- pkg/agent/baker.go
+++ pkg/agent/baker.go
@@ -1197,7 +1197,7 @@ root = "{{GetDataDir}}"{{- end}}
     {{- end}}
     {{- if IsKrustlet }}
     [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.spin]
-      runtime_type = "io.containerd.spin-v0-3-0.v1"
+      runtime_type = "io.containerd.spin-v0-15-1.v2"
     [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.slight]
       runtime_type = "io.containerd.slight-v0-3-0.v1"
     [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.spin-v0-3-0]
@@ -1214,6 +1214,8 @@ root = "{{GetDataDir}}"{{- end}}
       runtime_type = "io.containerd.slight-v0-8-0.v1"
     [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.wws-v0-8-0]
       runtime_type = "io.containerd.wws-v0-8-0.v1"
+    [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.spin-v0-15-1]
+      runtime_type = "io.containerd.spin-v0-15-1.v2"
     {{- end}}
   {{- if and (IsKubenet) (not HasCalicoNetworkPolicy) }}
   [plugins."io.containerd.grpc.v1.cri".cni]
@@ -1309,7 +1311,7 @@ root = "{{GetDataDir}}"{{- end}}
       BinaryName = "/usr/bin/runc"
     {{- if IsKrustlet }}
     [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.spin]
-      runtime_type = "io.containerd.spin-v0-3-0.v1"
+      runtime_type = "io.containerd.spin-v0-15-1.v2"
     [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.slight]
       runtime_type = "io.containerd.slight-v0-3-0.v1"
     [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.spin-v0-3-0]
@@ -1326,6 +1328,8 @@ root = "{{GetDataDir}}"{{- end}}
       runtime_type = "io.containerd.slight-v0-8-0.v1"
     [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.wws-v0-8-0]
       runtime_type = "io.containerd.wws-v0-8-0.v1"
+    [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.spin-v0-15-1]
+      runtime_type = "io.containerd.spin-v0-15-1.v2"
     {{- end}}
   {{- if and (IsKubenet) (not HasCalicoNetworkPolicy) }}
   [plugins."io.containerd.grpc.v1.cri".cni]
